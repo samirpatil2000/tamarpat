bb65d02b45f44b3fa323b86580e75161f91aea0a
samir patil <samirspatil742099@gmail.com>
2020-09-18 02:17:02 +0530

    register , login and logout form is added with email address

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,9 @@ from django import views
 from . import views
 #
 urlpatterns = [
-#    path('',views.index,name='home'),
-
+    path('',views.index,name='home'),
+    path('register/',views.registration_view,name='register'),
+    path('logout/',views.logout_view,name='logout'),
+    path('login/',views.login_view,name='login'),
 
  ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth import login, authenticate, logout
+from account.forms import RegistrationForm, AccountAuthenticationForm#, AccountUpdateForm
 
 # Create your views here.
+def index(request):
+    return render(request,'account/home.html')
+
+
+def registration_view(request):
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            return redirect('home')
+        else:
+            context['registration_form'] = form
+
+    else: #GET request
+        form = RegistrationForm()
+        context['registration_form'] = form
+    return render(request, 'account/register.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+
+def login_view(request):
+    context={}
+
+    user=request.user
+    if user.is_authenticated:
+        return redirect('home')
+
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("home")
+
+    else:
+        form = AccountAuthenticationForm()
+
+    context['login_form'] = form
+
+    # print(form)
+    return render(request, "account/login.html", context)
+
+
