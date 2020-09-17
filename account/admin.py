6243bc9b806628763b9bc3b8d9d0f03578bd96e0
samir patil <samirspatil742099@gmail.com>
2020-09-18 00:24:47 +0530

    customize django admin.py and search field

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Account
+from django.contrib.auth.admin import UserAdmin
+
+
+
+
+
+class AccountAdmin(UserAdmin):
+
+	list_display = ('email','username','date_joined', 'last_login', 'is_admin','is_staff')
+
+	search_fields = ('email','username',)
+
+	readonly_fields=('date_joined', 'last_login')  #the fields that can't be change
+
+	filter_horizontal = ()
+	list_filter = ()
+	fieldsets = ()
+
 
 # Register your models here.
+
+
+admin.site.register(Account, AccountAdmin)
