89c55ffe807e283ad2f484a51e1bc2ae889b5ae1
samir patil <samirspatil742099@gmail.com>
2021-06-27 18:28:44 +0530

    host

--- main1/settings.py
+++ main1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h$w6*!oaqq!@e61k333f&%_^(agl@^wh&k$ul^r0_-m^sculk8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
