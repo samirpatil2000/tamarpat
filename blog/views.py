011350dda84d91d1b2e8b642f6031a344ac9b226
samir patil <samirspatil742099@gmail.com>
2021-08-13 21:20:06 +0530

    pdf changes

--- blog/views.py
+++ blog/views.py
@@ -540,15 +540,16 @@ def addIndexTOThesisProject(request,proj_slug):
 
 
 def pdf(request):
-    obj=ThesisProject.objects.filter(is_checked=True)[0]
+    obj=ThesisProject.objects.filter(id=30)[0]
     context={
-        'pdf_':obj.pdf
+        'pdf_':'https://www.ph.ucla.edu/epi/rapidsurveys/RScourse/RSbook_ch3.pdf',
+        'pdf_2':obj.pdf,
     }
     return render(request,'new/control_pdf.html',context)
 
 def pdf2(request):
-    # obj=ThesisProject.objects.filter(is_checked=True)[0]
-    # context={
-    #     'pdf_':obj.pdf
-    # }
-    return render(request,'new/pdf.html')
+    obj=ThesisProject.objects.filter(id=30)[0]
+    context={
+        'pdf_':obj.pdf
+    }
+    return render(request,'new/pdf.html',context)
